dc56c0292cab5dddcff630a9bbb53cead9193f17
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-12-24 20:38:56 +0800

    feat: updated test on backend

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return {message: "Cronlite API v1.0.0 by DV8}', () => {
+      expect(appController.getHello()).toStrictEqual({
+        message: 'Cronlite API v1.0.0 by DV8',
+      });
     });
   });
 });
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { message: string } {
+    return { message: 'Cronlite API v1.0.0 by DV8' };
   }
 }
