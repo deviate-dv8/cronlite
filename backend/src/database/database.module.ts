a00620ad4c10ecb45a3ce19deafdea7a02a58445
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2026-02-17 03:42:07 +0800

    feat: updated database module to use global

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from 'src/config/typeorm.config';
+import { User } from './entities/User';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
       inject: [PostgresConfigService],
     }),
+    TypeOrmModule.forFeature([User]),
   ],
   controllers: [],
   providers: [],
-  exports: [],
+  exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
