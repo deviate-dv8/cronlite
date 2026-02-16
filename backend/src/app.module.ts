71b1a0b6603a5a139ff3ee10d7f9d5b51d724ef6
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2026-02-17 03:43:29 +0800

    feat: updated module and main.ts to the updated appConfig

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './health/health.controller';
-import configuration from './config/configuration';
+import { AuthModule } from './authentication/auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppConfigModule } from './config/appConfig.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-      isGlobal: true,
-    }),
-    DatabaseModule,
-  ],
+  imports: [AppConfigModule, DatabaseModule, AuthModule, UsersModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
